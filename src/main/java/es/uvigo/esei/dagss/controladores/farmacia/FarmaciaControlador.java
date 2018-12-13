1914894b4748324f8f9e8a784e3b6a1b8643c933
Nacho <rodriguezpereiranacho@gmail.com>
2018-12-13 20:33:08 +0100

    Avance

--- src/main/java/es/uvigo/esei/dagss/controladores/farmacia/FarmaciaControlador.java
+++ src/main/java/es/uvigo/esei/dagss/controladores/farmacia/FarmaciaControlador.java
@@ -15,6 +15,15 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
+//Imports nuevos
+import es.uvigo.esei.dagss.dominio.entidades.Paciente;
+import es.uvigo.esei.dagss.dominio.entidades.Prescripcion;
+import es.uvigo.esei.dagss.dominio.entidades.Receta;
+import es.uvigo.esei.dagss.dominio.daos.PacienteDAO;
+import es.uvigo.esei.dagss.dominio.daos.PrescripcionDAO;
+import es.uvigo.esei.dagss.dominio.daos.RecetaDAO;
+import java.util.List;
+
 /**
  *
  * @author ribadas
@@ -27,11 +36,25 @@ public class FarmaciaControlador implements Serializable {
     private String nif;
     private String password;
 
+     //Atributos nuevos
+    private Paciente pacienteActual;
+    private List<Receta> recetas;
+    private List<Prescripcion> prescripciones;
+    private String numeroTarjetaSanitaria;
+    
     @Inject
     private AutenticacionControlador autenticacionControlador;
 
     @EJB
     private FarmaciaDAO farmaciaDAO;
+    
+    //Injects nuevos
+    @Inject
+    private RecetaDAO recetaDAO;
+    @Inject
+    private PrescripcionDAO prescripcionDAO;
+        @Inject
+    private PacienteDAO pacienteDAO;
 
     /**
      * Creates a new instance of AdministradorControlador
@@ -87,6 +110,48 @@ public class FarmaciaControlador implements Serializable {
             }
         }
         return destino;
+    }
+    
+     public String gotoRecetas(){
+         
+        String destino;
+        
+        Paciente paciente = pacienteDAO.buscarPorTarjetaSanitaria(numeroTarjetaSanitaria);
+        
+        if (paciente == null) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "No existe un paciente con targeta sanitaria: " + numeroTarjetaSanitaria, ""));
+            destino =  "BuscarReceta";
+        } else {
+            pacienteActual = paciente;
+            recetas = recetaDAO.buscarPorIdPacienteConPrescripcion(pacienteActual.getId());
+            destino = "listaRecetas";
+        }
+         
+        return destino;
+    }
+     
+     public String doShowReceta(List<Receta> recetas){
+        this.recetas=recetas;
+        return "detalleReceta";
+    }
+    
+    public List<Receta> getRecetas(){
+        return recetas;
+    }
+    public List<Prescripcion> getPrescripciones(){
+        return prescripciones;
+    }
+    
+    public String getNumeroTarjetaSanitaria() {
+        return numeroTarjetaSanitaria;
+    }
+    
+    public void setNumeroTarjetaSanitaria(String numeroTarjetaSanitaria) {
+        this.numeroTarjetaSanitaria = numeroTarjetaSanitaria;
+    }
+    
+    public Paciente getPaciente(){
+        return pacienteActual;
     }
 
 }
